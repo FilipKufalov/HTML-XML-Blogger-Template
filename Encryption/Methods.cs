dc4d11af592f4431949605c1d406e8b96d3d165b
FilipKufalov <filip.kufalov@hotmail.com>
2022-11-06 01:24:17 +0100

    encrypt/decrypt

---
+++ Encryption/Methods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    public class Methods
+    {
+        public char[] Encrypt(string param)
+        {
+            char[] proc;
+            proc = param.ToCharArray();
+            char[] encryptedData = new char[proc.Length];
+            for (int i = 0; i < proc.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int data = proc[i] + 10;
+                    encryptedData[i] = (char)data;
+                }
+                else if (i % 3 == 0)
+                {
+                    int data = proc[i] + 5;
+                    encryptedData[i] = (char)data;
+                }
+                else
+                {
+                    int data = proc[i] + 3;
+                    encryptedData[i] = (char)data;
+                }
+            }
+            return encryptedData;
+        }
+
+        public char[] Decrypt(string param)
+        {
+            char[] proc;
+            proc = param.ToCharArray();
+            char[] deEncryptedData = new char[proc.Length];
+            for (int i = 0; i < proc.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int data = proc[i] - 10;
+                    deEncryptedData[i] = (char)data;
+                }
+                else if (i % 3 == 0)
+                {
+                    int data = proc[i] - 5;
+                    deEncryptedData[i] = (char)data;
+                }
+                else
+                {
+                    int data = proc[i] - 3;
+                    deEncryptedData[i] = (char)data;
+                }
+            }
+            return deEncryptedData;
+        }
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,13 @@
+﻿using Encryption;
+Methods method = new Methods();
+
+Console.Write("Type password: ");
+string password = Console.ReadLine();
+string encrypted_password = new string(method.Encrypt(password));
+string decrypted_password = new string(method.Decrypt(encrypted_password));
+
+Console.WriteLine($"Encrypted password: {encrypted_password}");
+Console.WriteLine($"Decrypted password: {decrypted_password}");
+
+
+
